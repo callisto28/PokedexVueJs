b9e5bb4b78bbc5b68589400184c8d6081aff8a89
seb-azot <sebastien.fourny@azot.dev>
2022-08-12 18:11:24 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container fluid>
+        <InstructionsTest />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import InstructionsTest from './components/InstructionsTest.vue';
+
+export default {
+  name: 'App',
+
+  components: {
+    InstructionsTest,
+  },
+
+
+};
+</script>
